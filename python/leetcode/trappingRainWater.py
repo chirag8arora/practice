4365f625390540317cedde41706030ff0344f54b
Xin Zhou <xinzhou918@gmail.com>
2014-06-02 22:56:42 -0400

    add candy, window substring

--- python/leetcode/trappingRainWater.py
+++ python/leetcode/trappingRainWater.py
@@ -3,17 +3,27 @@ class Solution:
     # @return an integer
 
     def trap(self, A):
+        start = 0
+        total = 0
+        trapped = 0
+        total = 0
+        start_index = -1
+        for k, i in enumerate(A):
+            if i >= start:
+                total += trapped
+                trapped = 0
+                start = i
+                start_index = k
+            else:
+                trapped += start - i
+        if trapped != 0:
+            next = [max(A[start_index + 1:])]
+            next.extend(A[start_index + 1:])
+            total += self.trap(next)
+        return total
 
 
 if __name__ == '__main__':
-    A = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-    print A
-    print  [i - 1 for i in A]
-    print  [i - 2 for i in A]
-    print  [i - 3 for i in A]
-    # [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-    # [0, 0, 0, 1, 0, 0, 0, 2, 1, 0, 1, 0]
-    # [0, 0, 0, 1, 0, 0, 0, 2, 1, 0, 1, 0]
-    # [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+    A = [4, 2, 3]
     s = Solution()
     print s.trap(A)
