f36a5a0b35e519a61bc6171a60a12248c4b0f8ef
Charlie <xinzhou918@gmail.com>
2014-09-01 14:54:29 -0400

    add max profit buying stock i, ii

--- python/leetcode2/best_time_to_buy_and_sell_stock.py
+++ python/leetcode2/best_time_to_buy_and_sell_stock.py
@@ -1,14 +1,27 @@
 # coding=utf-8
 # AC Rate: 31.0%
 # SOURCE URL: https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock/
-# 
+#
 # Say you have an array for which the ith element is the price of a given stock on day i.
 # If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
-# 
+#
 
 
 class Solution:
     # @param prices, a list of integer
     # @return an integer
     def maxProfit(self, prices):
-        
+        diff = [0]
+        for i in range(1, len(prices)):
+            diff.append(prices[i] - prices[i-1])
+
+        best = 0
+        current = 0
+        for i in diff:
+            current += i
+            if best < current:
+                best = current
+            if current < 0:
+                current = 0
+        return best
+
--- python/leetcode2/best_time_to_buy_and_sell_stock_ii.py
+++ python/leetcode2/best_time_to_buy_and_sell_stock_ii.py
@@ -1,14 +1,21 @@
 # coding=utf-8
 # AC Rate: 36.8%
 # SOURCE URL: https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
-# 
+#
 # Say you have an array for which the ith element is the price of a given stock on day i.
 # Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
-# 
+#
 
 
 class Solution:
     # @param prices, a list of integer
     # @return an integer
     def maxProfit(self, prices):
-        
+        profit = 0
+        for k, i in enumerate(prices):
+            if k >= 1:
+                if i - prices[k - 1] > 0:
+                    profit += i - prices[k - 1]
+        return profit
+
+
