5ee15097bc4a5d0d5ba695bf75757495d4f28b67
Charlie <xinzhou918@gmail.com>
2014-10-16 14:31:13 -0400

    add nlp viterbi alg and leetcode findMin 10/15/14

--- leetcode/best_time_to_buy_and_sell_stock_ii.py
+++ leetcode/best_time_to_buy_and_sell_stock_ii.py
@@ -1,9 +1,14 @@
 # coding=utf-8
 # AC Rate: 36.8%
-# SOURCE URL: https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+# SOURCE URL:
+# https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 #
-# Say you have an array for which the ith element is the price of a given stock on day i.
-# Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+# Say you have an array for which the ith element is the price of a given
+# stock on day i.
+# Design an algorithm to find the maximum profit. You may complete as many
+# transactions as you like (ie, buy one and sell one share of the stock
+# multiple times). However, you may not engage in multiple transactions
+# at the same time (ie, you must sell the stock before you buy again).
 #
 
 
@@ -11,11 +16,3 @@ class Solution:
     # @param prices, a list of integer
     # @return an integer
     def maxProfit(self, prices):
-        profit = 0
-        for k, i in enumerate(prices):
-            if k >= 1:
-                if i - prices[k - 1] > 0:
-                    profit += i - prices[k - 1]
-        return profit
-
-
